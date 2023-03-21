0cb82f74ff25417e043141d7b37a1d7e4b864a37
christian <christianbiringanine22@gmail.com>
2023-03-21 16:19:46 +0200

    Initialize the cube component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Cube from './components/cube';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <button className='btn btn-primary'>Click Me</button>
+      <Cube />
     </div>
   );
 }
--- src/components/cube.tsx
+++ src/components/cube.tsx
@@ -1,1 +1,17 @@
-export {}
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { Box } from '@react-three/drei';
+
+const Cube: React.FC = () => {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box>
+        <meshStandardMaterial attach="material" />
+      </Box>
+    </Canvas>
+  );
+};
+
+export default Cube;
