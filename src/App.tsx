6e71d628308ed72706c22bd738e85ecd2c38bf9b
christian <christianbiringanine22@gmail.com>
2023-03-22 22:18:09 +0200

    Iplement the cube component and ender it on the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as THREE from 'three';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
 import Cube from './components/cube';
 import './App.css';
 
 function App() {
+  const [cubeWidth, setCubeWidth] = useState(0);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+
+      setCubeWidth(data.value);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  console.log(cubeWidth);
   return (
     <div className="App">
-      <button className='btn btn-primary'>Click Me</button>
-      <Cube />
+      <Canvas camera={{ position: [0, 0, 10] }} onCreated={({ gl }) => {
+        gl.setClearColor(new THREE.Color("#f0f0f0"));
+      }} >
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <spotLight intensity={0.8} position={[300, 300, 300]} />
+        <Cube value={cubeWidth} />
+      </Canvas>
     </div>
   );
 }
--- src/components/cube.tsx
+++ src/components/cube.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import * as THREE from 'three';
 import { Canvas } from 'react-three-fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
 import { Box } from '@react-three/drei';
 
-const Cube: React.FC = () => {
+interface ShapeProps {
+  value: number;
+}
+
+const Cube: React.FC<ShapeProps> = ({ value }) => {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box>
-        <meshStandardMaterial attach="material" />
-      </Box>
-    </Canvas>
+    <mesh position={[0, 0, 0]}>
+      <boxGeometry args={[value, value, value]} />
+      <meshNormalMaterial />
+    </mesh>
   );
 };
 
