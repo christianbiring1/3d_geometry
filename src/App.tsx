45887231fd3a3e948065983f3baa9d242a5417af
christian <christianbiringanine22@gmail.com>
2023-03-22 23:37:15 +0200

    Improve the react render effeciency

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import * as THREE from 'three';
 import { Canvas } from 'react-three-fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
 import Cube from './components/cube';
 import './App.css';
 
+const MemoizedCube = React.memo(Cube);
+
 function App() {
   const [cubeWidth, setCubeWidth] = useState(0);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  const handleSocketMessage = useCallback((event: MessageEvent) => {
+    const data = JSON.parse(event.data);
+
+    setCubeWidth(data.value);
+  }, []);
+
+  const sockeUrl = useMemo(() => `ws://${window.location.hostname}:8080`, []);
+
 
   useEffect(() => {
-    const socket = new WebSocket("ws://localhost:8080");
-    socket.onmessage = (e) => {
-      const data = JSON.parse(e.data);
+    const socket = new WebSocket(sockeUrl);
+    socket.binaryType = 'arraybuffer';
 
-      setCubeWidth(data.value);
+    socket.onmessage = handleSocketMessage;
+
+    socket.onopen = () => {
+      console.log('WebSocket connection open');
     };
+
+    socket.onclose = () => {
+      console.log('WebSocket connection closed');
+      setSocket(null);
+    };
+    // socket.onmessage = (e) => {
+    //   const data = JSON.parse(e.data);
+
+    //   setCubeWidth(data.value);
+    // };
+
+    setSocket(socket);
 
     return () => {
       socket.close();
     };
-  }, []);
+  }, [sockeUrl, handleSocketMessage]);
+
+  const cubeProps = useMemo(() => ({ value: cubeWidth }), [cubeWidth]);
 
   console.log(cubeWidth);
   return (
@@ -31,7 +59,8 @@ function App() {
         <Stars />
         <ambientLight intensity={0.5} />
         <spotLight intensity={0.8} position={[300, 300, 300]} />
-        <Cube value={cubeWidth} />
+        <MemoizedCube {...cubeProps} />
+        {/* <Cube value={cubeWidth} /> */}
       </Canvas>
     </div>
   );
